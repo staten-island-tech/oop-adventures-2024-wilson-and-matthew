9de6d66461ba0758e0f5d3ff3d33138dfa308a57
matthewleesiths <matthewl497@nycstudents.net>
2025-01-07 09:22:15 -0500

    ijijf89h8yw9igo-khgij0
    fokjt190qgihyqhij0qh

---
+++ all.py
@@ -0,0 +1,83 @@
+class Maincharacter:
+    def __init__(self, name, inventory, money, hp, attack):
+        self.name = name
+        self.inventory = inventory
+        self.money = money
+        self.hp = hp
+        self.attack = attack
+        self.is_alive = True
+
+    def buy(self, item, money):
+        self.inventory.append(item)
+        print(self.inventory)
+        self.inventory.remove(money)
+        
+    def attack_enemy(self, enemy):
+        damage = max(0, self.attack)  # Basic attack calculation
+        print(f"{self.name} attacks {enemy.name} for {damage} damage!")
+        enemy.take_damage(damage)
+
+    def take_damage(self, eattack):
+        self.health -= eattack
+        print(f"{self.name} takes {eattack} damage!")
+        if self.health <= 0:
+            self.is_alive = False
+            self.die()
+            # print(f"{self.name} has been defeated!")
+
+    def die(self):
+        print(f"{self.name} has died! hahaha loser")
+        self.lose_items()
+
+    def lose_items(self):
+        items_to_keep = [special]
+
+        self.inventory = [item for item in self.inventory if item in items_to_keep]
+
+        self.money = 0
+        
+        print(f"{self.name} has died and lost all their stuff hahahaha")
+        print(f"Remaining items: {self.inventory}")
+        print(f"Remaining money: {self.money}")
+
+
+
+
+
+inventory = {
+
+}
+
+special = {
+
+}
+
+
+
+player_name = input("Enter your character's name: ")
+player = Maincharacter(player_name, inventory=[], money=50, hp=100, attack=5, speed=5)
+
+class Enemy:
+    def __init__(self, ename, ehp, eattack, espeed):
+        self.name = ename
+        self.ehp = ehp
+        self.eattack = eattack
+        self.espeed = espeed
+        self.is_alive = True
+
+    # def attack(self, player):
+    #     if self.is_alive:
+    #         print(f"{self.name} attacks {player.name} for {self.attack_damage} damage!")
+    #         player.take_damage(self.attack_damage)
+
+    def attack(self, player):
+        if self.is_alive:
+            damage = self.eattack
+            print(f"{self.name} attacks {player.name} for {damage} damage!")
+            player.take_damage(damage)
+
+
+    def move(self, direction):
+        print(f"{self.name} moves {direction} with a speed of {self.speed}.")
+ename1 = "cube"
+enemy = Enemy(ename1, ehp=100, eattack=5, espeed=10)
--- enemy.py
+++ enemy.py
@@ -1,8 +1,9 @@
 class Enemy:
-    def __init__(self, ename, ehp, eattack):
+    def __init__(self, ename, ehp, eattack, espeed):
         self.name = ename
         self.ehp = ehp
         self.eattack = eattack
+        self.espeed = espeed
         self.is_alive = True
 
     # def attack(self, player):
@@ -19,4 +20,99 @@ class Enemy:
 
     def move(self, direction):
         print(f"{self.name} moves {direction} with a speed of {self.speed}.")
+ename1 = "cube"
+enemy = Enemy(ename1, ehp=100, eattack=5, espeed=10)
+###########################################################################################################################################################################################
+###########################################################################################################################################################################################
+###########################################################################################################################################################################################
+import pygame
+import time
+import math
 
+TILE_SIZE = 32
+RED = (255, 0, 0)
+screen = pygame.display.set_mode((800, 600)) 
+
+class Projectile:
+    def __init__(self, x, y, direction_x, direction_y, speed):
+        self.x = x
+        self.y = y
+        self.width = 10
+        self.height = 10
+        self.color = (0, 255, 0)  #
+        self.speed = speed 
+        self.direction_x = direction_x
+        self.direction_y = direction_y
+
+    def update(self):
+        self.x += self.direction_x * self.speed
+        self.y += self.direction_y * self.speed
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+
+class Enemy:
+    def __init__(self, ename, x, y, ehp, eattack, espeed, erate):
+        self.name = ename
+        self.x = x
+        self.y = y
+        self.width = TILE_SIZE
+        self.height = TILE_SIZE
+        self.color = RED
+        self.ehp = ehp
+        self.eattack = eattack
+        self.speed = espeed 
+        self.erate = erate  
+        self.is_alive = True
+        self.projectiles = []
+        self.last_shot_time = time.time()
+
+    def attack(self, player):
+        if self.is_alive:
+            damage = self.eattack
+            print(f"{self.name} attacks {player.name} for {damage} damage!")
+            player.take_damage(damage)
+
+"""     def move(self, direction):
+        if direction == 'up':
+            self.y -= 1
+        elif direction == 'down':
+            self.y += 1
+        elif direction == 'left':
+            self.x -= 1
+        elif direction == 'right':
+            self.x += 1
+        print(f"{self.name} moves {direction}.") """
+
+def shoot(self, player):
+        current_time = time.time()
+        if current_time - self.last_shot_time > (1 / self.erate):  
+            direction_x = player.x - self.x
+            direction_y = player.y - self.y
+            distance = math.sqrt(direction_x**2 + direction_y**2)
+            direction_x /= distance
+            direction_y /= distance
+            projectile = Projectile(self.x + self.width // 2, self.y + self.height // 2, direction_x, direction_y, self.speed)
+            self.projectiles.append(projectile)
+            self.last_shot_time = current_time
+
+def update_projectiles(self):
+        for projectile in self.projectiles:
+            projectile.update()  
+
+def draw(self):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        for projectile in self.projectiles:
+            projectile.draw()  
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.hp = 100
+
+    def take_damage(self, damage):
+        self.hp -= damage
+        print(f"{self.hp} HP remaining.")
+
+monster = Enemy(ename="Monster", x=100, y=100, ehp=100, eattack=5, espeed=8, erate=2)  # Example monster with 8 speed for bullets and 2 shots per second
